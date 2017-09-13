a9a2a986b915aaaa89d630192b0e5203b80d593b
Lusey77 <lusek@uni.edu>
2017-09-12 22:00:08 -0500

    (comp): Created base home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { ApiService } from "./api-service";
 import { HttpModule } from "@angular/http";
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule, routingComponents } from "./app.routing";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    routingComponents
+    routingComponents,
+    HomeComponent
   ],
   imports: [
     BrowserModule, HttpModule, AppRoutingModule
