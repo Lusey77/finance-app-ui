ccff551ad172746d4563d657aebe8d36d19336b6
Lusey77 <lusek@uni.edu>
2017-09-13 21:27:52 -0500

    (wip): Added routing to home component and changed background color

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountComponent } from "./account/account.component";
 import { TransactionComponent } from "./transaction/transaction.component";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'Account'},
   {path: '**', pathMatch: 'full', redirectTo: 'Account'},
   {path: 'Accounts', component: AccountComponent},
   {path: 'Transactions', component: TransactionComponent},
+  {path: 'Home', component: HomeComponent},
 ];
 
 @NgModule({
@@ -16,4 +18,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule {}
 
-export const routingComponents = [AccountComponent, TransactionComponent];
+export const routingComponents = [AccountComponent, TransactionComponent, HomeComponent];
