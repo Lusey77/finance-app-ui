d7508458be1a791c40cb6219dd810a7345991a13
Lusey77 <Lusey77@users.noreply.github.com>
2017-10-17 21:57:24 -0500

    Changed app.component.html in index.js to index.html

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/app-component.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
