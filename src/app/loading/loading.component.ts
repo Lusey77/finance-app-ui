339b7208e30a808472580152f6c5bd9cc228f92c
Lusey77 <Lusey77@users.noreply.github.com>
2018-01-26 20:04:40 -0600

    Committing current changes

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,23 +1,87 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: [
-    '../../../node_modules/primeng/resources/themes/omega/theme.css',
-    '../../../node_modules/primeng/resources/primeng.min.css',
-    '../../../node_modules/font-awesome/css/font-awesome.min.css',
-    '../../../node_modules/bootstrap/dist/css/bootstrap.min.css',
     './loading.component.css',
-    '../shared.css'
+    '../../../node_modules/font-awesome/css/font-awesome.min.css'
   ],
   encapsulation: ViewEncapsulation.Native
 })
 export class LoadingComponent implements OnInit {
+  @Input() icon: string = 'fa-spinner';
+  @Input() picture: string;
+  @Input() position: number = 5;
+  @Input() modal: boolean = true;
+  @Input() visible: boolean = true;
+  @Input() height: number;
+  @Input() width: number;
+  @Input() fill: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getIcon() {
+    if (!this.picture) {
+      return this.icon;
+    }
+  }
+
+  getHeight() {
+    if (this.fill) {
+      return '100%';
+    }
+
+    if (this.height) {
+      return this.height + 'px';
+    }
+  }
+
+  getWidth() {
+    if (this.fill) {
+      return '100%';
+    }
+
+    if (this.width) {
+      return this.width + 'px';
+    }
+  }
+
+  iconPosition() {
+    switch (this.position) {
+      case 1: {
+        return 'posTopLeft';
+      }
+      case 2: {
+        return 'posTopCenter';
+      }
+      case 3: {
+        return 'posTopRight';
+      }
+      case 4: {
+        return 'posMiddleLeft';
+      }
+      case 5: {
+        return 'posMiddleCenter';
+      }
+      case 6: {
+        return 'posMiddleRight';
+      }
+      case 7: {
+        return 'posBottomLeft';
+      }
+      case 8: {
+        return 'posBottomCenter';
+      }
+      case 9: {
+        return 'posBottomRight';
+      }
+      default: {
+        return 'posMiddleCenter';
+      }
+    }
+  }
 }
